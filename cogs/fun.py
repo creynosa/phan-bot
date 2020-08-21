9dab0905ea2c7de2cf90142b26e2e42a4eea5400
Cesar Reynosa <phantastro@gmail.com>
2020-08-20 21:11:24 -0500

    Added snitch bot feature.

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,6 +14,30 @@ class Fun(commands.Cog):
             embed = discord.Embed(description=string, color=0x01a8ef)
         return embed
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        import re
+
+        if message.author.id == self.bot.user.id:
+            return
+        
+        discordPingRE = re.compile(r'<@!\d{18}>')
+        messageText = message.content
+
+        if re.search(discordPingRE, messageText) is not None:
+            try:
+                pingingUser = message.author
+
+                embed = discord.Embed(
+                    color=0x01a8ef,
+                    description=f'{pingingUser.mention} just pinged someone! :eyes:'
+                    )
+                
+                await message.channel.send(embed=embed)
+            except:
+                return
+
+
     @commands.command()
     async def caprikachu(self, ctx):
         embed = Fun.embedMessage('https://i.imgur.com/aqe7pfU.png')
