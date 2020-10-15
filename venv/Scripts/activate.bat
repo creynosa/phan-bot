c8c0d36def77d001435840e88f41c5ced5f73d77
Phan <phantastro@gmail.com>
2020-10-15 06:58:33 -0500

    Remake venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Phan\Documents\Programming\Discord\Bots\only-phans-bot\venv
+set VIRTUAL_ENV=C:\Users\Phan\Documents\Programming\Discord\Bots\phan-bot\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
