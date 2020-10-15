018b66d931928b0f17839c174be3f64fe0b368d0
Phan <phantastro@gmail.com>
2020-10-15 07:01:52 -0500

    Format the code using black.

--- bot.py
+++ bot.py
@@ -2,21 +2,22 @@ import os
 from discord.ext import commands
 
 # Main Bot Token
-TOKEN = os.environ['only-phans-bot-token']
-bot = commands.Bot(command_prefix='!')
+TOKEN = os.environ["only-phans-bot-token"]
+bot = commands.Bot(command_prefix="!")
+
 
 @bot.event
 async def on_ready():
     """Print a console message when the bot is ready and active."""
-    print('Logged in as')
+    print("Logged in as")
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    print("------")
 
 
 # Loads all cogs when ready
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py') and filename != '__init__.py':
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f"cogs.{filename[:-3]}")
 
 
