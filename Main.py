1d87c0270fac0e8d056170bd311c755cb4c8ddca
Phan <phantastro@gmail.com>
2020-11-05 18:22:04 -0600

    Edit main.py to allow for presences.

--- Main.py
+++ Main.py
@@ -1,27 +1,29 @@
 import os
+import discord
 from discord.ext import commands
 
 # Main Bot Token
-TOKEN = os.environ['phan-bot-token']
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+
+TOKEN = os.environ["phan-bot-token"]
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
 async def on_ready():
     """Print a console message when the bot is ready and active."""
-    print('Logged in as')
+    print("Logged in as")
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
+    print("------")
 
 
 # Loads all cogs when ready
-for filename in os.listdir('cogs'):
-    if filename.endswith('.py') and filename != '__init__.py':
-        if filename in ['fractals.py']:
-            pass
-        else:
-            bot.load_extension(f"cogs.{filename[:-3]}")
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py") and filename != "__init__.py":
+        bot.load_extension(f"cogs.{filename[:-3]}")
 
 
 @bot.event
